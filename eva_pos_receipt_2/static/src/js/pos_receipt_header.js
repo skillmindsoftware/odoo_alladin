8a21a8f6f997eb990a95e460472585494b31bd27
Elvice <83223687+elvoceouma@users.noreply.github.com>
2024-07-26 09:20:57 +0300

    removed total qty

--- eva_pos_receipt_2/static/src/js/pos_receipt_header.js
+++
@@ -1,6 +0,0 @@
-/** @odoo-module */
-import { ReceiptHeader } from 'point_of_sale.ReceiptHeader';
-
-export class CustomReceiptHeader extends ReceiptHeader {
-    static template = 'point_of_sale.ReceiptHeader';
-}
